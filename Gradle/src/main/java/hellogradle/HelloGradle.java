37baf37e1673dec2b3c179dc993ff554be97a60e
Suraj Nimankar <surajnimankar@gmail.com>
2017-09-26 14:14:07 +0530

    First Gradle project

---
+++ Gradle/src/main/java/hellogradle/HelloGradle.java
@@ -0,0 +1,13 @@
+package hellogradle;
+
+import org.joda.time.LocalTime;
+
+public class HelloGradle {
+
+    public static void main(String[] args) {
+        LocalTime time = new LocalTime();
+        System.out.println("Local time is: " + time);
+        System.out.println("Hello Gradle..!");
+    }
+
+}
